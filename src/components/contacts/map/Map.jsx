c8b0f9873c9790f443efb2cf5294dad1557d5bf5
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2024-03-25 11:13:22 +0200

    add contacts section

---
+++ src/components/contacts/map/Map.jsx
@@ -0,0 +1,4 @@
+export default function Map(){
+
+    return(<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2670448.8036641986!2d27.123223522964118!3d49.1842401175679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dc673dfa85bb03%3A0x7e675cd9074d3f4a!2z0JfQsNC_0L7RgNGW0LbQttGPLCDQl9Cw0L_QvtGA0ZbQt9GM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNjkwMDA!5e0!3m2!1suk!2sua!4v1711355173349!5m2!1suk!2sua" width="400" height="300"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import About from "@/components/about/About";
 import Skills from "@/components/skills/Skills";
 import Scroll from "@/components/skroll/Skrol";
 import Education from "@/components/education/Education";
+import Projects from "@/components/project/Projects";
+import Contacts from "@/components/contacts/Contacts";
+
 export default function HomePage (){
     return(< div className={styles.HomePage}>
     <Scroll/>
@@ -14,6 +17,8 @@ export default function HomePage (){
     <About/>
     <Skills/>
     <Education/>
+    {/* <Projects/> */}
+    <Contacts/>
     </div>
     </div>)
 }
